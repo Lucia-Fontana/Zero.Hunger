e337d1a7a7ba54de5e46149cd1fcd2ba75f2e760
Federica Rossi <federicarossi22@hotmail.com>
2022-12-06 10:51:03 +0000

    first fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category, :description, :deadline_for_consumption, :photo)
+  end
 end
