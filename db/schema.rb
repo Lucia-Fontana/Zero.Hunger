93e44cdb0fa14fd562a24c5d1eaf571d6fdd38b1
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-12-12 16:25:24 +0100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_12_12_103142) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_134049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,9 +71,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_103142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
-    t.boolean "availability", default: true
     t.integer "quantity"
     t.float "CO2e"
+    t.boolean "availability", default: true
     t.index ["business_id"], name: "index_products_on_business_id"
   end
 
