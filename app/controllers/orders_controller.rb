ef8e81db8f6f0726a537da27dbbce573990fb040
guillaume drevo <guillaume.drevo@gmail.com>
2022-12-12 19:30:35 +0100

    fixed order confirmation, now purchases are ordered by location

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,35 @@ class OrdersController < ApplicationController
   def show
     @user = current_user
     @order = current_user.orders.find(params[:id])
+    @allpurchases = @order.purchases
+    @locations = []
+    @purchases = []
+    @allpurchases.each do |purchase|
+      address = purchase.product.business.address
+      unless @locations.include?(address)
+        @locations << address
+      end
+    end
+    @address1_articles = []
+    @address2_articles = []
+    @address3_articles = []
+    @address4_articles = []
+    @address5_articles = []
+    @address6_articles = []
+    @address1 = @locations[0]
+    @address2 = @locations[1] unless @locations[1].nil?
+    @address3 = @locations[2] unless @locations[2].nil?
+    @address4 = @locations[3] unless @locations[3].nil?
+    @address5 = @locations[4] unless @locations[4].nil?
+    @address6 = @locations[5] unless @locations[5].nil?
+    @allpurchases.each do |purchase|
+      @address1_articles << purchase if purchase.product.business.address == @address1
+      @address2_articles << purchase if purchase.product.business.address == @address2
+      @address3_articles << purchase if purchase.product.business.address == @address3
+      @address4_articles << purchase if purchase.product.business.address == @address4
+      @address5_articles << purchase if purchase.product.business.address == @address5
+      @address6_articles << purchase if purchase.product.business.address == @address6
+    end
   end
 
   def new
