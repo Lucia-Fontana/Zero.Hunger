0e14f61c07865fd881650f18e9c667bff422d3fd
Federica Rossi <federicarossi22@hotmail.com>
2022-12-14 11:10:38 +0000

    updates

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,9 @@
 class PaymentsController < ApplicationController
   def new
-    @order = current_user.orders.where(state: 'pending').find(params[:order_id])
+    if current_user.orders.where(state: 'pending').empty?
+      @order = Order.create(user: current_user)
+    else
+      @order = current_user.orders.where(state: 'pending').find(params[:order_id])
+    end
   end
 end
